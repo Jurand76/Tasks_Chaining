d20a230d7f250b4aea7cf1aa77d3fba01f9d7b6f
Jurand76 <jurand@interia.pl>
2022-06-12 12:53:32 +0200

    Dodaj pliki projektów.

---
+++ Tasks_Chaining/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks_Chaining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task task1 = new Task(CreateArray10integers);
+
+            Console.WriteLine("Hello World!");
+        }
+
+        public static Array CreateArray10integers()
+        {
+            Random randInt = new Random();
+            int[] array = Enumerable.Repeat(0, 10)
+                                    .Select(i => randInt.Next(int.MinValue, int.MaxValue)).ToArray();
+            return array;
+        }
+    }
+}
